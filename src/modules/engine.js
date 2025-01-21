9d30c47e6eff787fe38422beb704d33465851ac3
Behindtheboard <jk2624@gmail.com>
2025-01-20 16:53:52 -0800

    Add receiveAttack Gameboard class method

--- src/modules/engine.js
+++ src/modules/engine.js
@@ -36,6 +36,7 @@ class Ship {
 class Gameboard {
   constructor() {
     this.board = this.createBoard(10);
+    this.fleet = []
   }
 
   createBoard(n) {
@@ -47,25 +48,49 @@ class Gameboard {
     const col = coordinate[1];
     const length = ship.length;
     if (Number(row) + length > 9 || Number(col) + length > 9) {
-      return console.error('out of bounds')
+      return console.error("out of bounds");
     }
-    let i;
+    this.fleet.push(ship)
     if (ship.isVertical) {
       for (let i = row; i <= ship.length; i++) {
-        this.board[i][col] = ship.name;
+        this.board[i][col] = ship;
       }
     } else {
       for (let i = col; i <= ship.length; i++) {
-        this.board[row][i] = ship.name;
+        this.board[row][i] = ship;
       }
     }
   }
+
+  recieveAttack(coordinate) {
+    const hitBox = this.board[coordinate[0]][coordinate[1]];
+    console.log(hitBox);
+    if (hitBox !== null) {
+      hitBox.hit();
+      return (this.board[coordinate[0]][
+        coordinate[1]
+      ] = `hit ${hitBox.name[0]}`);
+    } else {
+      if (hitBox === "hit") {
+        return console.error("already attacked");
+      }
+      this.board[coordinate[0]][coordinate[1]] = "missed";
+    }
+  }
+
+  fleetSunk() {
+
+  }
 }
+
+
 
 const carrier = new Ship("carrier", false);
 const test = new Gameboard();
 
-test.placeShip("12", carrier);
+test.placeShip("00", carrier);
+test.recieveAttack("00");
+
 console.log(test.board);
 
 module.exports = { Ship, Gameboard };
